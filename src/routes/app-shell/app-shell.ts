02b08daf9748c554d6b8225e08d00df8a5138aef
injune1123 <injune1123@pivotal-guest-5-252.ny.pivotallabs.com>
2016-03-04 18:37:06 -0500

    added the address

--- src/routes/app-shell/app-shell.ts
+++ src/routes/app-shell/app-shell.ts
@@ -1,10 +1,11 @@
 import {Component} from "angular2/core";
 import {CardComponent} from "../../components/card/card";
+import {ContInfoComponent} from "../../components/contInfo/contInfo";
 
 @Component({
     selector: 'app-shell',
     template: require('./app-shell.html'),
-    directives: [CardComponent]
+    directives: [CardComponent, ContInfoComponent]
 })
 export class AppShell {
 
